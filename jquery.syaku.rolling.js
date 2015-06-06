378370ebf57e68e8f53ddc60e8575c0dcd829e54
syaku <syaku@naver.com>
2015-06-07 03:43:44 +0900

    작업중...

--- jquery.syaku.rolling.js
+++ jquery.syaku.rolling.js
@@ -25,7 +25,7 @@
 			'items': null,
 			'width': null,
 			'height': null,
-			'frame': 100,
+			'frame': 500,
 			'delay': 1000,
 			'move': 'left'
 
@@ -35,6 +35,29 @@
 			var O = $.extend(true, { }, _jrolling.options, options);
 			O = $.extend({ }, _jrolling, { 'options': O });
 			return O;
+		},
+
+		'first2last': function(arr) {
+			var b = arr.shift();
+			arr.push(b);
+			return arr;
+		},
+
+		'last2first': function(arr) {
+			var b = arr.pop();
+			arr.unshift(b);
+			return arr;
+		},
+
+		'order': function(arr, index1, index2) {
+			if (index2 >= arr.length) {
+				var k = index2 - arr.length;
+				while ((k--) + 1) {
+					arr.push(undefined);
+				}
+			}
+			arr.splice(index2, 0, arr.splice(index1, 1)[0]);
+			return arr; 
 		}
 	};
 
@@ -47,7 +70,7 @@
 		this.target = object.target;
 		var $target = this.target;
 		var $items = $(object.options.items);
-		var $count = $items.length;
+		var $size = $items.length;
 		var $index = 0;
 		var $timer = null;
 
@@ -55,55 +78,129 @@
 		var $height = parseInt(object.options.height);
 
 		$target.css({
-			'position': 'relative',
+			'position': 'absolute',
 			'overflow': 'hidden'
 		});
 
-		var container_size = {};
-		var items_size = {};
-		switch(object.options.move) {
-			case 'left': 
-			case 'right': 
-				container_size = { 'width': ($width * $count) + 'px' };
-				items_size = { 'width': object.options.width };
-			break;
-			case 'up': 
-			case 'down': 
-				container_size = { 'height': ($height * $count) + 'px' };
-				items_size = { 'height': object.options.height };
-			break;
+		var $flow = [];
+
+		this.init = function() {
+			$items.each(function(i) {
+				$(this).css({
+					'position': 'absolute',
+					'left': '0px',
+					'top': '0px',
+					'width': object.options.width,
+					'height': object.options.height,
+					'z-index': 0
+				});
+
+				$flow.push(i);
+			});
+
+			// ---- ready 
+			//$flow = _jrolling.last2first($flow);
+			//this.prepara(true);
+
+			//console.log($flow, $index, $size);
+			//console.log('wait=============');
 		}
 
-		//var $container = $('<div style="position: absolute;overflow:hidden;"></div>').css(container_size);
-		//$target.append($container);
-		//$container.append($items);
+		this.prepara = function(ready) {
+			//console.log('size', $size, $index);
 
-		$items.each(function(i) {
-			$(this).css({ 'position': 'absolute' , 'left': ($width * i) + 'px' }).css(items_size);
-		});
+			$items.each(function(i) {
 
-		var fn = {
+				// $flow 첫번째는 * -1 두번째는 0 : 이지만 이동하지 않음 애니매이션 처리.
+				// 이전에 첫번째 배열은 효과를 주지 않는 다. (마지막은 효과를 주지 않는 다.)
+				var index = $flow.indexOf(i);
+				var v = null;
+				switch(index) {
+					case 0: 
+						v  = -1; 
+					break;
+					case 1: 
+						v = 0; 
+					break;
+					default:
+						v = index  - 1;
+					break;
+				}
+				var distance = $width * v;
+/*
+				var animate = true;
+				if (next == true) {
+					if (index < 2) animate = false;
+				} else if(next == false) {
+					if (($size-2) < index) animate = false;
+				}
+*/
+				console.log($size, i, index, v, distance);
 
-			'move': function() {
-				$target.append( $items.first() );
-				$items.animate({ 'left': '-=' + object.options.width }, { 'duration': object.options.frame, 'step': function(x, t) {
-					//var t = $(this);
-					console.log(t);
-					//if (t.index() == 0) 
+				$(this).css({ 'left': distance });
 
-				} });
-			},
-			'prepara': function() {
-				$target.append($items.first());
-				//$items = $(object.options.items);
-				//$(object.options.items).first().css({ 'left': '0px' });
-				$(object.options.items).first().next().css({ 'left': object.options.width });
-			}
-		};
+				//if (ready != true && ($size-1) != index) {
+/*				if (ready != true) {
+					$(this).animate({ 'left': distance }, object.options.frame);
+				} else {
+					$(this).css({ 'left': distance });
+				}*/
+				//if (index < 1 && ready != true) {
+				//	$(this).animate({ 'left': distance }, object.options.frame);
+				//} else {
+				//	$(this).css({ 'left': distance });
+				//}
+
+				
+			});
+
+			//return now;
+		}
+
+		this.prev = function() {
+			$flow = _jrolling.last2first($flow);
+			$index++;
+			if ($size == $index) $index = 0;
+		}
+		
+		this.next = function() {
+			$flow = _jrolling.first2last($flow);
+			$index++;
+			if ($size == $index) $index = 0;
+
+			$items.animate({ 'left': '-=' + object.options.width }, object.options.frame);
+		}
+
+		this.motion = function() {
+
+			/*
+			1. 준비 : 왼쪽에서 오른쪽 위에서 아래로 좌표 지정
+			2. 방향에 따른 아이템 위치 선정.
+				-> 이동전에 마지막 아이템을 처음으로
+				배열($flow) 이동
+				애니 작동
+				<- 이동후에 처음 아템을 마지막으로
+			
+			2. 정리
+			*/
+
+			if ($index == 1) $this.stop();
+
+			// 이동방향
+			$this.next();
+
+			// 방향에 맞는 위치 선정
+			$this.prepara(false);
+			
+			// 애니메이션
+			console.log($flow, $index);
+			console.log('move=============');
+			//$this.prepara().animate({ 'left': 0 }, object.options.frame);
+		}
 
 		this.play = function() {
-			fn.move();
-			//$timer = setInterval(fn.move, object.options.delay);
+			//fn.motion();
+			$timer = setInterval(this.motion, object.options.delay);
 		}
 
 		this.stop = function() {
@@ -126,7 +223,7 @@
 		object.target = this;
 
 		var instance = new jrolling( object );
-		//instance.init();
+		instance.init();
 		instance.play();
 
 		return instance;
